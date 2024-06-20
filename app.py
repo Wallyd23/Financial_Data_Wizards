e4ecbb3207b5e5ad36f1c7f86df92e05f47330b4
Justin Lent <justin.s.lent@gmail.com>
2024-06-20 19:16:55 -0400

    updated flask app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # Import necessary libraries
 # Flask is a web framework, jsonify is used to convert objects to JSON
 from flask import Flask, jsonify  
+#SQLAlchemy
+from sqlalchemy import create_engine
 # PostgreSQL database adapter for Python
-import psycopg2 
+#import psycopg2 
 # pandas is a powerful data analysis and manipulation library 
 import pandas as pd  
 
@@ -10,33 +12,59 @@ import pandas as pd
 app = Flask(__name__)
 
 # Function to establish a connection to the PostgreSQL database
-def get_db():
-    conn = psycopg2.connect(
+#def get_db():
+    #conn = psycopg2.connect(
         # Name of your database
-        dbname="FinanceDB", 
+        #dbname="FinanceDB", 
         # Username to connect to the database
-        user="postgres",
+        #user="postgres",
         # Password to connect to the database
-        password="postgres",
+        #password="postgres",
         # Host where the database is running
-        host="localhost", 
+        #host="localhost", 
         # Port on which the database is running 
-        port="5433"  
-    )
+        #port="5433"  
+    #)
     # Return the connection object
-    return conn  
+    #return conn  
+
+# Function to establish a connection to the PostgreSQL database
+def get_db():
+    # SQLAlchemy connection string
+    conn_str = 'postgresql://postgres:postgres@localhost:5433/FinanceDB'
+    # Create SQLAlchemy engine
+    engine = create_engine(conn_str)
+    return engine
 
 # Define a route for your API
 @app.route('/api/data')
 def get_data():
     # Get the database connection using the function defined above
-    conn = get_db() 
+    #conn = get_db() 
+    # Get the database connection using the function defined above
+    engine = get_db()
     # SQL query to fetch data 
-    query = 'SELECT * FROM public."ETFprices";'
+    query = 'SELECT * FROM public."ETFprices" LIMIT 100;'
     # Execute the SQL query and load the results into a pandas DataFrame  
-    df = pd.read_sql_query(query, conn) 
+    df = pd.read_sql_query(query, engine)#conn) 
     # Convert the DataFrame to a dictionary and then to a JSON object 
     return jsonify(df.to_dict(orient='records'))  
+
+# Define ETF route
+@app.route('/api/etfprices')
+def get_etf_data():
+    engine = get_db()
+    query = 'SELECT * FROM public."ETFprices";'
+    df = pd.read_sql_query(query, engine)
+    return jsonify(df.to_dict(orient='records'))
+
+# Define index prices route
+@app.route('/api/index_prices')
+def get_index_prices_data():
+    engine = get_db()
+    query = 'SELECT * FROM public."IndexPrices" LIMIT 100;'
+    df = pd.read_sql_query(query, engine)
+    return jsonify(df.to_dict(orient='records'))
 
 # Main driver function
 if __name__ == '__main__':
