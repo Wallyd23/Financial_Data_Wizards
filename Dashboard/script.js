98578d1e87d4dc2499c2a78e37902378a89e4153
aradar1 <161782139+aradar1@users.noreply.github.com>
2024-06-23 19:31:48 -0400

    Final Changes

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -5,9 +5,37 @@ function changeDocument() {
   var selectedType = selectedValue.split('.').pop(); // Get the file extension
   if (selectedType === 'html') {
       // Use iframe for HTML files
-      container.outerHTML = `<iframe id=displayedDocument src=${selectedValue} style=“width: 600px; height: 400px;“></iframe>`;
+      container.outerHTML = `<iframe id=displayedDocument src=${selectedValue} style="width:600px; height:400px;"></iframe>`;
   } else if (selectedType === 'png') {
       // Use img tag for PNG images
-      container.outerHTML = `<img id=displayedDocument src=${selectedValue} alt=“Displayed Image” style=“width: 600px; height: auto;“>`;
+      container.outerHTML = `<img id=displayedDocument src=${selectedValue} alt="Displayed Image" style="width: 600px; height: auto;">`;
+  }
+}
+
+function changeIndex() {
+  var selector = document.getElementById('indexSelector');
+  var container = document.getElementById('displayedIndex');
+  var selectedValue = selector.value;
+  var selectedType = selectedValue.split('.').pop(); // Get the file extension
+  if (selectedType === 'html') {
+      // Use iframe for HTML files
+      container.outerHTML = `<iframe id=displayedIndex src=${selectedValue} style="width:600px; height:400px;"></iframe>`;
+  } else if (selectedType === 'png') {
+      // Use img tag for PNG images
+      container.outerHTML = `<img id=displayedIndex src=${selectedValue} alt="Displayed Image" style="width: 600px; height: auto;">`;
+  }
+}
+
+function changePrice() {
+  var selector = document.getElementById('priceSelector');
+  var container = document.getElementById('displayedPrice');
+  var selectedValue = selector.value;
+  var selectedType = selectedValue.split('.').pop(); // Get the file extension
+  if (selectedType === 'html') {
+      // Use iframe for HTML files
+      container.outerHTML = `<iframe id=displayedPrice src=${selectedValue} style="width:600px; height:400px;"></iframe>`;
+  } else if (selectedType === 'png') {
+      // Use img tag for PNG images
+      container.outerHTML = `<img id=displayedPrice src=${selectedValue} alt="Displayed Image" style="width: 600px; height: auto;">`;
   }
 }
