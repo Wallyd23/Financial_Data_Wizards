4d61eabd805eae8746a746a389a40cc39456b944
Jacksoler <23jsoler@gmail>
2024-06-23 18:37:16 -0400

    dropdown update

--- script.js
+++ script.js
@@ -1,66 +1,14 @@
-// This function is called when a dropdown menu item is selected
-function updatePlotly() {
-  // Use D3 to select the dropdown menu
-  let dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  let dataset = dropdownMenu.property("value");
+function changeDocument() {
+  var selector = document.getElementById('documentSelector');
+  var container = document.getElementById('displayedDocument');
+  var selectedValue = selector.value;
+  var selectedType = selectedValue.split('.').pop(); // Get the file extension
 
-  // Initialize x and y arrays
-  let x = [];
-  let y = [];
-
-  if (dataset === 'djiafigure') {
-    x = [1, 2, 3, 4, 5];
-    y = [1, 2, 4, 8, 16];
+  if (selectedType === 'html') {
+      // Use iframe for HTML files
+      container.outerHTML = `<iframe id="displayedDocument" src="${selectedValue}" style="width: 600px; height: 400px;"></iframe>`;
+  } else if (selectedType === 'png') {
+      // Use img tag for PNG images
+      container.outerHTML = `<img id="displayedDocument" src="${selectedValue}" alt="Displayed Image" style="width: 600px; height: auto;">`;
   }
-
-  else if (dataset === 'nasdaqfigure') {
-    x = [10, 20, 30, 40, 50];
-    y = [1, 10, 100, 1000, 10000];
-  }
-
-  // Add else if conditions for each of your HTML files and PNG images
-  else if (dataset === 'AAXJfigure') {
-    // Load the HTML file
-    d3.select("#plot").html('<object data="Notebooks/AAXJfigure.html"/>');
-  }
-  else if (dataset === 'AADRfigure') {
-    // Load the HTML file
-    d3.select("#plot").html('<object data="Notebooks/AADRfigure.html"/>');
-  }
-  else if (dataset === 'ZIVZfigure') {
-    // Load the HTML file
-    d3.select("#plot").html('<object data="Notebooks/ZIVZfigure.html"/>');
-  }
-  else if (dataset === 'ZROZfigure') {
-    // Load the HTML file
-    d3.select("#plot").html('<object data="Notebooks/ZROZfigure.html"/>');
-  }
-  else if (dataset === 'ZSLfigure') {
-    // Load the HTML file
-    d3.select("#plot").html('<object data="Notebooks/ZSLfigure.html"/>');
-  }
-  else if (dataset === 'nysefigure') {
-    // Load the HTML file
-    d3.select("#plot").html('<object data="Notebooks/nysefigure.html"/>');
-  }
-  else if (dataset === 'sp500figure') {
-    // Load the HTML file
-    d3.select("#plot").html('<object data="Notebooks/sp500figure.html"/>');
-  }
-  else if (dataset === 'Indexprices') {
-    // Load the HTML file
-    d3.select("#plot").html('<object data="Notebooks/Indexprices.png');
-  }
-  else if (dataset === 'etfprices') {
-    // Display the PNG image
-    d3.select("#plot").html('<img src="Notebooks/etfprices.png" alt="ETF Prices">');
-  }
-  // Continue with else if conditions for the remaining HTML files and PNG images
-
-  // Note the extra brackets around 'x' and 'y'
-  Plotly.restyle("plot", "x", [x]);
-  Plotly.restyle("plot", "y", [y]);
 }
-
-init();
